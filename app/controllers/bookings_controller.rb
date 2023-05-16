f6432ee91c093344d1d34cd7ebf0601158b373e5
fontain richardson <fontainwr@gmail.com>
2023-05-16 14:12:25 +0900

    I added two methods to the controller and created two html.erb files for them

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def create
+    @kitchen = kitchen.find(params[:kitchen_id])
+    @booking = @kitchen.bookings.build(booking_params)
+    if @booking.save
+      #task: If a successful booking creating, re-drirect to the confirmation page.
+    else
+      #render an error page message.
+    end
+  end
+
+  def index
+    @bookings = Booking.where(chef_id: current_chef.id).order(start_date: :desc)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :guests)
+  end
+
 end
