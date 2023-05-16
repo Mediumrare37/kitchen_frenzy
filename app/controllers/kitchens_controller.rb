8cb450f03b2ea55ca2d611db77e1105f81cfe20f
Andrew Dembiczak <andrew.dembiczak@gmail.com>
2023-05-16 12:20:05 +0900

    Pundit installed and authorizations implemented

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,2 +1,38 @@
 class KitchensController < ApplicationController
+
+  def new
+    @kitchen = Kitchen.new
+    authorize @kitchen
+  end
+
+  def create
+    @kitchen = Kitchen.new(kitchen_params)
+    @kitchen.user = current_user
+    authorize @kitchen
+  end
+
+
+  def show
+    authorize @kitchen
+  end
+
+  def kitchen_params
+    params.require(:kitchen).permit(:name)
+  end
+
+  def edit
+    authorize @kitchen
+  end
+
+  def update
+    authorize @kitchen
+  end
+
+  def destroy
+    authorize @kitchen
+  end
+
+  def index
+    @kitchens = policy_scope(Kitchen)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings # bookings as a client
+  has_many :kitchens
+  has_many :bookings_as_owner, through: :kitchens, source: :bookings
 end
