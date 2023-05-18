266f16bbccb48f45f640951728619ebcf93affdc
Guilherme Hortinha <guihortinha@gmail.com>
2023-05-18 18:37:57 +0900

    finishehd owners bookings page, there is a bug in price

--- app/policies/owner/booking_policy.rb
+++ app/policies/owner/booking_policy.rb
@@ -4,26 +4,24 @@ class Owner::BookingPolicy < ApplicationPolicy
     def resolve
       scope.where(user: user)
     end
+  end
+  def show?
+    record.user == user
+  end
 
-    def show?
-      record.user == user
-    end
+  def create?
+    true
+  end
 
-    def create?
-      true
-    end
+  def edit?
+    record.user == user
+  end
 
-    def edit
-      record.user == user
-    end
+  def update?
+    record.kitchen.user == user
+  end
 
-    def update
-      record.user == user
-    end
-
-    def destroy
-      record.user == user
-    end
-
+  def destroy?
+    record.user == user
   end
 end
