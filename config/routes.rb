c6a8e8466f1b1ae2d996c5cd65ce13d8ff20435a
fontain richardson <fontainwr@gmail.com>
2023-05-18 18:17:13 +0900

    Added forms, edit, controller methods and added edit and delete functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :kitchens, only: [:index, :show, :new, :create] do
+
+  resources :kitchens, only: [:index, :show, :new, :create, :destroy, :edit] do
+    member do
+      delete 'kitchens/', action: :destroy
+    end
     resources :bookings, only: [:create]
   end
+
   resources :bookings, only: [:index, :update]
 
   namespace :owner do
     resources :bookings, only: [:index, :create]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Other routes...
 
   # Defines the root path route ("/")
   # root "articles#index"
