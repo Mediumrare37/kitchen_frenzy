2d47cdbca9edd03c2a4b9a5af2595747541edc94
Guilherme Hortinha <guihortinha@gmail.com>
2023-05-19 15:16:01 +0900

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create, :new]
   end
 
-  resources :bookings, only: [:index, :update] do
-  end
+  resources :bookings, only: [:index, :update]
 
   namespace :owner do
     resources :bookings, only: [:index, :create, :update]
